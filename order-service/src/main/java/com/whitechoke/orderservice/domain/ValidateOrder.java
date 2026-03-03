17c5dd1132ea2befe0ef7fdc51155c30da0bb702
WhiteChoke <denisduchenkoo@gmail.com>
2026-03-03 19:24:05 +0900

    created validation class for order

--- order-service/src/main/java/com/whitechoke/orderservice/domain/ValidateOrder.java
+++ order-service/src/main/java/com/whitechoke/orderservice/domain/ValidateOrder.java
@@ -1,4 +1,30 @@
 package com.whitechoke.orderservice.domain;
 
+import com.whitechoke.orderservice.api.dto.OrderCreateRequestDto;
+import org.mapstruct.ap.shaded.freemarker.template.utility.NullArgumentException;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ValidateOrder {
+
+    public void validateRequest(OrderCreateRequestDto request) {
+        if (request.customerName() == null) {
+            throw new NullArgumentException("Customer name cannot be null");
+        }
+        if (request.customerPhone() == null) {
+            throw new NullArgumentException("Customer phone cannot be null");
+        }
+        if (request.customerName().isBlank()) {
+            throw new IllegalArgumentException("Customer name cannot be empty");
+        }
+        if (request.customerPhone().isBlank()) {
+            throw new IllegalArgumentException("Customer phone cannot be empty");
+        }
+        if (request.deliveryType() == null){
+            throw new NullArgumentException("Delivery type cannot be null");
+        }
+        if (request.productIds().isEmpty()) {
+            throw new IllegalArgumentException("Picked products cannot be empty");
+        }
+    }
 }
