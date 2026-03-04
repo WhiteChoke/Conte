459c00bdccf82d8f8a2b80bfe2094edc6fbf82df
WhiteChoke <denisduchenkoo@gmail.com>
2026-03-04 17:40:14 +0900

    created http client for product

--- order-service/src/main/java/com/whitechoke/orderservice/domain/external/ProductHttpClient.java
+++ order-service/src/main/java/com/whitechoke/orderservice/domain/external/ProductHttpClient.java
@@ -1,4 +1,19 @@
 package com.whitechoke.orderservice.domain.external;
 
-public class ProductHttpClient {
+import com.whitechoke.api.http.variant.VariantResponseDto;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.List;
+
+@HttpExchange(
+        accept = "application/json",
+        contentType = "application/json",
+        url = "/api/v1/variants"
+)
+public interface ProductHttpClient {
+
+    @GetExchange(url = "ids")
+    List<VariantResponseDto> getVariantsByIds(@RequestBody List<Long> ids);
 }
--- order-service/src/main/java/com/whitechoke/orderservice/domain/external/ProductHttpClientConfig.java
+++ order-service/src/main/java/com/whitechoke/orderservice/domain/external/ProductHttpClientConfig.java
@@ -4,13 +4,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
-public class HttpClientConfig {
+public class ProductHttpClientConfig {
 
     @Value("${http-url.product-service}")
     private String productServiceUrl;
 
     @Bean
-    RestClient 
+    RestClient productRestClient() {
+        return RestClient.builder()
+                .baseUrl(productServiceUrl)
+                .build();
+    }
+
+    @Bean
+    ProductHttpClient productHttpClient() {
+        return HttpServiceProxyFactory.builder()
+                .exchangeAdapter(RestClientAdapter.create(productRestClient()))
+                .build()
+                .createClient(ProductHttpClient.class);
+    }
 }
